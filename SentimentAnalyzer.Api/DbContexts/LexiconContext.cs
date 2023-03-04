e55fca31f7d184c19910035999b02fffcc308b59
Александар Арсић <aa223044m@student.etf.bg.ac.rs>
2023-03-05 00:00:08 +0100

    Adding  Entity class, DbContext and registering EF Core into project

---
+++ SentimentAnalyzer.Api/DbContexts/LexiconContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SentimentAnalyzer.Api.Entities;
+
+namespace SentimentAnalyzer.Api.DbContexts
+{
+    public class LexiconContext : DbContext
+    {
+        public DbSet<Lexicon> Lexicon { get; set; }
+
+        public LexiconContext(DbContextOptions<LexiconContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Lexicon>()
+                .HasIndex(e => e.Word)
+                .IsUnique();
+        } 
+
+    }
+}
---
+++ SentimentAnalyzer.Api/Entities/Lexicon.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SentimentAnalyzer.Api.Entities
+{
+    public class Lexicon
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string? Word { get; set; }
+
+        [Required]
+        public float SentimentScore { get; set; }
+    }
+}
