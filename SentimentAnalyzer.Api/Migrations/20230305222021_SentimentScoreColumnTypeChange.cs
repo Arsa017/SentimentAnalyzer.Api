39b84de4457c40b410d1028bd4fd0a706ed4ea24
Александар Арсић <aa223044m@student.etf.bg.ac.rs>
2023-03-06 00:46:41 +0100

    Changing sentiment score column type

---
+++ SentimentAnalyzer.Api/Migrations/20230305222021_SentimentScoreColumnTypeChange.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SentimentAnalyzer.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SentimentScoreColumnTypeChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SentimentScore",
+                table: "Lexicon",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "SentimentScore",
+                table: "Lexicon",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
